2e3a6e18075b3ea180e9c30f7e90a3504f93ca6e
deeppaz <deeppaz@hotmail.com>
2019-10-23 16:08:54 +0300

    task20

---
+++ WebApp/Task20.js
@@ -0,0 +1,24 @@
+var getValueOfInput=function(i1){
+    document.getElementById("i1").value = "Hello text";
+}
+function check() {
+    document.getElementById("i2").checked = true;
+}
+function radiobutton() {
+    document.getElementById("i3").checked = true;
+}
+function dropdown() {
+    document.getElementById('i4').selectedIndex = 0;
+}
+function inputpassword() {
+    var pass = document.getElementById("i5");
+    if (pass.type === "password") {
+      pass.type = "text";
+    } else {
+      pass.type = "password";
+    }
+}
+
+function inputdatetime() {
+    document.getElementById("i6").value = "23.10:2019 - 16:04";
+}
