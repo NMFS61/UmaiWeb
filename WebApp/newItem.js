bb3ca0b209b53f60d17b6daebead056c08265449
deeppaz <deeppaz@hotmail.com>
2019-10-29 22:56:13 +0300

    task21 change image in modal

---
+++ WebApp/newItem.js
@@ -0,0 +1,25 @@
+var fileTag = document.getElementById("filetag"),
+    preview = document.getElementById("preview");
+    
+fileTag.addEventListener("change", function() {
+  changeImage(this);
+});
+
+function changeImage(input) {
+  var reader;
+
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+      preview.setAttribute('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function acceptedImage(){
+    var x = document.getElementById("demo").src;
+    document.getElementById("result").innerHTML = x;
+}
